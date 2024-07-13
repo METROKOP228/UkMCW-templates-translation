ebb2533da340b366feaf431f4a68d0b05635ecc2
Yurii <111344205+METROKOP228@users.noreply.github.com>
2024-07-13 13:29:35 -0500

    bug fix

--- js/translation.js
+++ js/translation.js
@@ -1,32 +1,66 @@
 const javaUrl = 'https://raw.githubusercontent.com/METROKOP228/UkMCW-templates-translation/main/files/java/1.21.txt';
-let translations_java;
-fileFetch(javaUrl, translations_java)
+let translations_java = [];
+
+fileFetch(javaUrl)
+    .then(data => {
+        translations_java = data;
+    })
+    .catch(error => {
+        console.error('Error fetching file:', error);
+    });
 
 const bedrockUrl = 'https://raw.githubusercontent.com/METROKOP228/UkMCW-templates-translation/main/files/bedrock/newest.txt';
-let translations_bedrock;
-fileFetch(bedrockUrl, translations_bedrock)
+let translations_bedrock = [];
+
+fileFetch(bedrockUrl)
+    .then(data => {
+        translations_bedrock = data;
+    })
+    .catch(error => {
+        console.error('Error fetching file:', error);
+    });
 
 const earthUrl = 'https://raw.githubusercontent.com/METROKOP228/UkMCW-templates-translation/main/files/earth/newest.txt';
-let translations_earth;
-fileFetch(earthUrl, translations_earth)
+let translations_earth = [];
+
+fileFetch(earthUrl)
+    .then(data => {
+        translations_earth = data;
+    })
+    .catch(error => {
+        console.error('Error fetching file:', error);
+    });
 
 const legendsUrl = 'https://raw.githubusercontent.com/METROKOP228/UkMCW-templates-translation/main/files/legends/newest.txt';
-let translations_legends;
-fileFetch(legendsUrl, translations_legends)
+let translations_legends = [];
+
+fileFetch(legendsUrl)
+    .then(data => {
+        translations_legends = data;
+    })
+    .catch(error => {
+        console.error('Error fetching file:', error);
+    });
 
 function fileFetch(url, array) {
-    fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok ' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            array = data;
-            array = array.split('\n');
-        })
-        .catch(error => console.error('Error fetching file:', error));
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.text();
+            })
+            .then(data => {
+                // Розділити отримані дані на масив рядків за символом '\n'
+                array = data.split('\n');
+                resolve(array); // Повертаємо масив рядків через resolve
+            })
+            .catch(error => {
+                console.error('Error fetching file:', error);
+                reject(error); // Передаємо помилку через reject у випадку невдалої вибірки
+            });
+    });
 }
 
 function translateNames() {
