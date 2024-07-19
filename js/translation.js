225d999c03a33d16ded754eb95eeb706d813e20a
Yurii <111344205+METROKOP228@users.noreply.github.com>
2024-07-19 10:38:23 -0500

    fixed a bug

--- js/translation.js
+++ js/translation.js
@@ -34,12 +34,12 @@ function translateJava(text) {
     let en_uk = [];
     try {
         for (let i = 0; i < text.length; i++) {
-            let matches;
+            let matches = [];
             if (text[i].includes("[[File:") || text[i].includes("[[Файл:")) {
                 matches = text[i].match(/\[\[(File|Файл):[^\]]*\]\]/g);
                 for (let j = 0; j < matches.length; j++) {
                     for (let match of matches) {
-                        text[i] = text[i].replace(match, 'ЗАМІНИТИ')
+                        text[i] = text[i].replace(match, 'ЗАМІНИТИ');
                     }
                 }
             }
@@ -51,14 +51,15 @@ function translateJava(text) {
             }
             if (matches.length > 0) {
                 for (let match of matches) {
-                    text[i] = text[i].replace('ЗАМІНИТИ', match)
+                    text[i] = text[i].replace('ЗАМІНИТИ', match);
                 }
             }
         }
         text = text.join("\n");
         return text;
     } catch (error) {
-        return error;
+        console.log(error);
+        return text;
     }
 }
 
